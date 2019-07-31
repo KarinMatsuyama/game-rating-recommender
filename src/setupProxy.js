013ab124e1b96cedbdb71e6cec90a15cfe36f0b7
KarinMatsuyama <m.karin2@gmail.com>
2019-07-31 15:54:34 -0500

    added additional routes in setupproxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,5 +3,6 @@ const proxy = require('http-proxy-middleware')
 module.exports = function(app) {
     app.use(proxy('/games', { target: 'https://api-v3.igdb.com', changeOrigin: true }))
     app.use(proxy('/covers', { target: 'https://api-v3.igdb.com', changeOrigin: true }))
-
+    app.use(proxy('/genres', { target: 'https://api-v3.igdb.com', changeOrigin: true }))
+    app.use(proxy('/platforms', { target: 'https://api-v3.igdb.com', changeOrigin: true }))
 }
